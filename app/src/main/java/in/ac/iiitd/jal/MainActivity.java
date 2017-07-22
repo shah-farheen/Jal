63892ce65b1d8904068859832fa153343adad448
shah-farheen <farheenshahdelhi@gmail.com>
2017-07-23 01:06:52 +0530

    Initial commit

---
+++ app/src/main/java/in/ac/iiitd/jal/MainActivity.java
@@ -0,0 +1,20 @@
+package in.ac.iiitd.jal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference dataRef = database.getReference("TestKey");
+        dataRef.setValue("Test Value");
+    }
+}
